bac40d48509d6c16f2686743049238df3a447f1c
lee <sipossa@naver.com>
2022-08-05 10:30:12 +0900

    20220805

---
+++ project/src/doc/Section04.js
@@ -0,0 +1,23 @@
+const Section04 = () => {
+    return (
+        <section className='About02 basicSection'>
+            <h2><span>CEO's</span>GREETINGS</h2>
+            <p>항상 새로운 우투드영투드우</p>
+            <div className="inner">
+                <div className="top">
+                    <figure>
+                        <img src="./img/main01.jpg" alt="" />
+                    </figure>
+                </div>
+                <div className="left">
+                    50%
+                </div>
+                <div className="right">
+                    50%
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section04;
